5249df882ee0de93e42899ff011f01cf93eafaf6
Espen Gunnarsen <espen.gunnarsen@gmail.com>
2021-02-19 10:35:30 +0100

    Delete how_to_lambdas/hello_lambda_old directory

--- how_to_lambdas/hello_lambda_old/lambda/handler.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-
-    # See Weber, page 80
-    if "body" in event:
-        return {"response": event["body"] + "_Jalla!"}
-    else:
-        return {"response": "No body found."}
-
