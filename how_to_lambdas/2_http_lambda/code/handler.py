46fe59e1b23a4bbbba4735df5735a0d20ce5f8af
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2021-02-20 22:35:33 +0100

    Handle POST-requests.

--- how_to_lambdas/2_http_lambda/code/handler.py
+++ how_to_lambdas/2_http_lambda/code/handler.py
@@ -3,19 +3,36 @@ import json
 # Make a simple GET call by pasting the URL in the browser like e.g.:
 # https://07xnk0295b.execute-api.eu-west-1.amazonaws.com/dev/hello/Espen 
 
+
 def hello_get(event, context):
+
+    """
+    Test:
+    curl https://07xnk0295b.execute-api.eu-west-1.amazonaws.com/dev/hello/Espen
+    """
 
     name = event["pathParameters"]["name"]  # name is specified as a parameter in serverless.yml
 
-    response = {"greeting": f"Hello, {name}"}
+    response = {"greeting": f"Hello, {name}! (Reply after GET)"}
 
     return {"statusCode": 200,
-            "body": json.dumps(response),
-            "headers": {"Content-type": "application/json"}
+            "headers": {"Content-type": "application/json"},
+            "body": json.dumps(response)
             }
 
 
-
 def hello_post(event, context):
-    return {"msg": "Work in progress...."}
 
+    """
+    Test (https://medium.com/100-days-of-linux/10-curl-commands-that-you-should-know-ee3d032eb351)
+    curl -d '{"name": "Espen"}' https://07xnk0295b.execute-api.eu-west-1.amazonaws.com/dev/hello  # This works
+    curl -d 'name=Espen' https://07xnk0295b.execute-api.eu-west-1.amazonaws.com/dev/hello  # This does NOT work. WHY?
+
+    """
+    name = json.loads(event["body"])["name"]
+    response = {"greeting": f"Hello, {name}! (Reply after POST)"}
+
+    return {"statusCode": 200,
+            "headers": {"Content-type": "application/json"},
+            "body": json.dumps(response)
+            }
