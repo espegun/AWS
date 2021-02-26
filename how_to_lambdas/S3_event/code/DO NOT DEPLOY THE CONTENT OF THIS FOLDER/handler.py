57ce4e83c0011eeb375e38501202c8f8483cbab4
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2021-02-26 15:17:01 +0100

    After the great cost scandal.

--- how_to_lambdas/S3_event/code/DO NOT DEPLOY THE CONTENT OF THIS FOLDER/handler.py
+++ how_to_lambdas/S3_event/code/DO NOT DEPLOY THE CONTENT OF THIS FOLDER/handler.py
@@ -3,10 +3,16 @@ import time
 
 import boto3
 
+# https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html
+# Also see this example, where the lambda is given a role which has a policy which
+# also has the permissions required for the lambda to read and write to certain S3 buckets.
 
-def read_and_write_filesize(event, context):
+
+def react_to_file_event(event, context):
 
     BUCKET = "aws-basics"
+
+    print("Running reach_to_file_event.")
 
     s3 = boto3.resource("s3", region_name="eu-west-1")
     obj = s3.Object(BUCKET, "dump.txt")
